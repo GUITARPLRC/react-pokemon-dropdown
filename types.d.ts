efb24c9b4c262daaf2b62e8a9b00928025221b96
Chuck Reynolds <guitarplrc@yahoo.com>
2024-04-06 14:06:32 -0500

    feat: adjust type

--- types.d.ts
+++ types.d.ts
@@ -1,29 +1,30 @@
-// PokeDrop.d.ts
-
-interface Pokemon {
-  id: number
-  name: string
-}
-
-interface DropProps {
-  clearIcon?: () => JSX.Element
-  dropdownIcon?: () => JSX.Element
-  noResultsText?: string
-  onChange?: (value: string) => void
-  placeholder?: string
-  showId?: boolean
-  style?: {
-    container?: React.CSSProperties
-    input?: React.CSSProperties
-    searchInput?: React.CSSProperties
-    icon?: React.CSSProperties
-    dropdown?: React.CSSProperties
-    option?: React.CSSProperties
-    optionText?: React.CSSProperties
-    noOption?: React.CSSProperties
-    noOptionText?: React.CSSProperties
+declare module "poke-drop" {
+  export interface Pokemon {
+    id: number
+    name: string
   }
-  value?: string
-}
 
-declare const PokeDrop: React.FC<DropProps>
+  export interface DropProps {
+    clearIcon?: () => JSX.Element
+    dropdownIcon?: () => JSX.Element
+    noResultsText?: string
+    onChange?: (value: string) => void
+    placeholder?: string
+    showId?: boolean
+    style?: {
+      container?: React.CSSProperties
+      input?: React.CSSProperties
+      searchInput?: React.CSSProperties
+      icon?: React.CSSProperties
+      dropdown?: React.CSSProperties
+      option?: React.CSSProperties
+      optionText?: React.CSSProperties
+      noOption?: React.CSSProperties
+      noOptionText?: React.CSSProperties
+    }
+    value?: string
+  }
+
+  const PokeDrop: React.FC<DropProps>
+  export default PokeDrop
+}
